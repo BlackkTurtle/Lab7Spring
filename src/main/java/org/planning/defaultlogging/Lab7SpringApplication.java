be09cb1d06bb3bb0b831d7eaa270b72c7f77618a
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2023-09-26 18:46:02 +0300

    Initial commit

---
+++ src/main/java/org/planning/defaultlogging/Lab7SpringApplication.java
@@ -0,0 +1,13 @@
+package org.planning.defaultlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/planning/defaultlogging/Lab7SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.planning.defaultlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
